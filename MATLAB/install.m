8f68b8908379d49790e5e6b5604ccb2fc2b7a243
Peter Carbonetto <pcarbo@uchicago.edu>
2012-03-20 13:38:19 -0500

    Fixed install file for MATLAB code.

--- MATLAB/install.m
+++ MATLAB/install.m
@@ -1,15 +1,22 @@
 % This is a small script to compile the necessary MEX files.
-mex -O -largeArrayDims COPTIMFLAGS="-std=gnu99 -O3" LDOPTIMFLAGS="-O3" ...
-    ../C/var1matlab.c ../C/doublevectormatlab.c ../C/singlematrixmatlab.c ...
-    ../C/vectorops.c
-mex -O -largeArrayDims COPTIMFLAGS="-std=gnu99 -O3" LDOPTIMFLAGS="-O3" ...
-    ../C/diagsqmatlab.c ../C/diagsq.c ../C/doublevectormatlab.c ...
-    ../C/singlematrixmatlab.c ../C/vectorops.c
+cflags  = '-O3 -Os -std=gnu99';
+ldflags = '-O3 -Os -s';
+options = sprintf('-O -largeArrayDims COPTIMFLAGS="%s" LDOPTIMFLAGS="%s"',...
+		  cflags,ldflags);
+sourcefiles = {' ../C/doublevectormatlab.c'
+	       ' ../C/singlematrixmatlab.c' 
+	       ' ../C/sigmoid.c'
+	       ' ../C/vectorops.c'
+	       ' ../C/diagsq.c'
+	       ' ../C/diagsqt.c'
+	       ' ../C/varbvs.c'
+	       ' ../C/varbvsbin.c' };
+eval(['mex ',options,' ../C/var1matlab.c',sourcefiles{[1 2 4]}]);
+eval(['mex ',options,' ../C/varbvsupdatematlab.c',sourcefiles{[1:4 7]}]);
+eval(['mex ',options,' ../C/diagsqmatlab.c',sourcefiles{[1 2 4 5]}]);
+fprintf('Compilation of MEX files is complete.\n');
+
 % mex -O -largeArrayDims CXXOPTIMFLAGS='-O3' LDOPTIMFLAGS='-O3' ...
 %     diagsqtfast.cpp doublevector.cpp singlematrix.cpp common.cpp
-mex -O -largeArrayDims COPTIMFLAGS="-std=gnu99 -O3" LDOPTIMFLAGS="-O3" ...
-    ../C/varbvsupdatematlab.c ../C/varbvs.c ../C/doublevectormatlab.c ...
-    ../C/singlematrixmatlab.c ../C/vectorops.c ../C/sigmoid.c
 % mex -O -largeArrayDims CXXOPTIMFLAGS='-O3' LDOPTIMFLAGS='-O3' ...
 %     varbvsbinupdate.cpp doublevector.cpp singlematrix.cpp common.cpp
-fprintf('Compilation of MEX files is complete.\n');
