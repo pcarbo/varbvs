3ca443ef446e269b30489b5dc3d3281b2ca1e464
Peter Carbonetto <pcarbo@uchicago.edu>
2014-03-18 14:50:48 -0500

    Fixed a mistake in the comments for pve2sa.m.

--- MATLAB/pve2sa.m
+++ MATLAB/pve2sa.m
@@ -2,7 +2,6 @@
 % the outcome given the prior estimate of the proportion of variance
 % explained by the additive effects (H). THETA0 is the (base 10) logarithm
 % of the prior odds for inclusion, and SX is the sum of the sample variances
-% for all the explanatory variables. If inputs SA and THETA0 are both not
-% scalars, they must be numeric arrays of the same dimension.
+% for all the explanatory variables.
 function sa = pve2sa (sx, h, theta0)
   sa = h./(1-h)./(sigmoid10(theta0) * sx);
