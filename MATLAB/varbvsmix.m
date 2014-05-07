dc8029a94c98c5256a17aa3711d3b7745384bc32
Peter Carbonetto <pcarbo@uchicago.edu>
2014-05-07 11:35:27 -0500

    Fixed the comments for VARBVSMIX.

--- MATLAB/varbvsmix.m
+++ MATLAB/varbvsmix.m
@@ -1,11 +1,9 @@
-% *** FIX THESE COMMENTS ***
-% 
-% [LNZ,ALPHA,MU,S] = VARBVS(X,Y,SIGMA,SA,LOGODDS) implements the
-% fully-factorized variational approximation for Bayesian variable selection
-% in linear regression. It finds the "best" fully-factorized variational
-% approximation to the posterior distribution of the coefficients in a
-% linear regression model of a continuous outcome (quantitiative trait),
-% with spike and slab priors on the coefficients. By "best", we mean the
+% [LNZ,ALPHA,MU1,MU2,S1,S2,SIGMA] = VARBVSMIX(X,Y,SIGMA,SA1,SA2,LOGODDS)
+% implements the fully-factorized variational approximation for the "mixture
+% model" with linear regression. It finds a "good" fully-factorized
+% variational approximation to the posterior distribution of the
+% coefficients in a linear regression model of a continuous outcome, with
+% normal mixture priors on the coefficients. By "good", we mean the
 % approximating distribution that locally minimizes the Kullback-Leibler
 % divergence between the approximating distribution and the exact posterior.
 %
@@ -14,46 +12,31 @@
 % samples, and P is the number of variables. Y is the vector of observations
 % about the outcome; it is a vector of length N. To account for an
 % intercept, Y and X must be centered beforehand so that Y and each column
-% of X has a mean of zero. 
+% of X has a mean of zero. Note that this function is not implemented quite
+% as efficiently as VARBVS and VARBVSBIN to handle large matrices X.
 %
-% Note that this routine is implemented with the assumption that the data X
-% is single floating-point precision (type HELP SINGLE), as opposed to the
-% MATLAB default of double precision. This is useful for large data sets,
-% because single precision requires half of the number of bits as double
-% floating-point precision. If X is provided in another numerical
-% representation, it is immediately converted to SINGLE.
+% Inputs SA1, SA2 and LOGODDS are the fixed hyperparameters, in which SA and
+% SA2 are scalars. SIGMA is a scalar specifying an initial estimate for the
+% variance of the residual. SA1*SIGMA is the prior variance for coefficients
+% drawn from the first mixture component, and SA2*SIGMA is the prior
+% variance for coefficients drawn from the second mixture component. LOGODDS
+% is the prior log-odds that a coefficient is drawn from the first mixture
+% component. LOGODDS may either be a scalar, in which case all the variables
+% have the same prior inclusion probability, or it may be a vector of length
+% equal to P.
 %
-% Inputs SIGMA, SA and LOGODDS are the hyperparameters. SIGMA and SA are
-% scalars. SIGMA specifies the variance of the residual, and SA*SIGMA is the
-% prior variance of the coefficients. LOGODDS is the prior log-odds of
-% inclusion for each variable. It is equal to LOGODDS = LOG(Q./(1-Q)), where
-% Q is the prior probability that each variable is included in the linear
-% model of Y (i.e. the prior probability that its coefficient is not zero).
-% LOGODDS may either be a scalar, in which case all the variables have the
-% same prior inclusion probability, or it may be a vector of length P.
-%
-% There are four outputs. Output scalar LNZ is the variational estimate of
-% the marginal log-likelihood given the hyperparameters SIGMA, SA and
-% LOGODDS.
-%
-% Outputs ALPHA, MU and S are the parameters of the variational
-% approximation and, equivalently, variational estimates of posterior
-% quantites: under the variational approximation, the ith regression
-% coefficient is normal with probability ALPHA(i); MU(i) and S(i) are the
-% mean and variance of the coefficient given that it is included in the
-% model. Outputs ALPHA, MU and S are all column vectors of length P.
+% There are seven outputs: output scalar LNZ is the variational estimate of
+% the marginal log-likelihood given the hyperparameters; outputs ALPHA, MU1,
+% MU2, S1 and S2 are the parameters of the variational approximation and,
+% equivalently, variational estimates of posterior quantites. Outputs ALPHA,
+% MU1, MU2, and S1 and S2 are all column vectors of length P. The final
+% output is the maximum likelihood estimate of the residual variance, SIGMA.
 %
 % VARBVS(...,OPTIONS) overrides the default behaviour of the algorithm. Set
-% OPTIONS.ALPHA and OPTIONS.MU to override the random initialization of
-% variational parameters ALPHA and MU. Set OPTIONS.VERBOSE = FALSE to turn
-% off reporting the algorithm's progress. Set OPTIONS.UPDATE_VARS to specify
-% the indices of the variables to update, so that the variational estimates
-% for the other variables remain fixed. And set OPTIONS.UPDATE_SIGMA = TRUE
-% to compute the maximum likelihood estimate of the residual variance
-% (SIGMA), in which case input SIGMA acts as the initial estimate of this
-% parameter. When this option is set to TRUE, the maximum likelihood
-% estimate is returned in the fifth output.
-function [lnZ, alpha, mu1, mu2, s1, s2] = ...
+% OPTIONS.ALPHA, OPTIONS.MU1 and OPTIONS.MU2 to override the random
+% initialization of variational parameters ALPHA, MU1 and MU2. And set
+% OPTIONS.VERBOSE = FALSE to turn off reporting the algorithm's progress.
+function [lnZ, alpha, mu1, mu2, s1, s2, sigma] = ...
     varbvsmix (X, y, sigma, sa1, sa2, logodds, options)
 
   % Convergence is reached when the maximum relative distance between
@@ -169,8 +152,6 @@ function [lnZ, alpha, mu1, mu2, s1, s2] = ...
     % parameter (SIGMA). Note that after updating the residual variance
     % parameter, we must also recalculate the variance of the regression
     % coefficients. 
-    %
-    % TO DO: verify this expression.
     sigma = (norm(y - Xr)^2 + d'*betavarmix(alpha,mu1,mu2,s1,s2) ...
              + alpha'*(s1 + mu1.^2)/sa1 ...
              + (1-alpha)'*(s2 + mu2.^2)/sa2)/(n + p);
