01051395aa6acc0907ca567e660efecc20ec15f0
Peter Carbonetto <pcarbo@uchicago.edu>
2014-05-23 11:43:07 -0500

    Created a couple of new MATLAB functions, INDBVS and SINGLESNPHYPER, to calculate "single-marker" posterior probabilities when we ignore correlations between all the markers (i.e. variables).

---
+++ MATLAB/indbvs.m
@@ -0,0 +1,15 @@
+% [ALPHA,MU,S] = INDBVS(X,Y,SIGMA,SA,LOGODDS) computes posterior
+% probabilities and expectations of the coefficients in Bayesian variable
+% selection assuming that all variables are independent of each other. See
+% VARBVS and VARBVSUPDATE for details about the inputs and outputs.
+function [alpha, mu, s] = indbvs (xy, d, sigma, sa, logodds)
+
+  % Calculate the mean (mu) and variance of the coefficients (s) given that
+  % the coefficients are included in the model, then calculate the posterior
+  % inclusion probabilities (alpha), ignoring correlations between all
+  % variables (or features).
+  s     = sa*sigma./(sa*d + 1);
+  mu    = s.*xy/sigma;
+  SSR   = mu.^2./s;
+  alpha = sigmoid(logodds + (log(s/(sa*sigma)) + SSR)/2);
+
---
+++ MATLAB/singlesnphyper.m
@@ -0,0 +1,43 @@
+% DESCRIPTION OF THIS FUNCTION GOES HERE.
+function [alpha, mu, s] = singlesnphyper (X, y, w, log10sigma, h, theta0)
+
+  % Get the number of participants in the study (n), the number of SNPs
+  % genotyped (p), and the number of combinations of the hyperparameters
+  % (ns). 
+  [n p] = size(X);
+  ns    = numel(h);
+
+  % Get the settings for the prior residual variance (sigma), and the prior
+  % variance of the additive effects (sa).
+  sigma = 10.^log10sigma;
+  sx    = sum(var1(X));
+  sa    = pve2sa(sx,h,theta0);
+
+  % Compute a couple useful quantities. Here I calculate X'*Y as (Y'*X)' to
+  % avoid storing the transpose of X, since X may be large.
+  xy = double(y'*X)';
+  d  = diagsq(X);
+
+  % Initialize storage for the posterior inclusion probabilities (alpha),
+  % posterior means (mu), and posterior variances (s) of the regression
+  % coefficients.
+  alpha = zeros(p,ns);
+  mu    = zeros(p,ns);
+  s     = zeros(p,ns);
+
+  % Repeat for each combination of the hyperparameters.
+  for i = 1:ns
+
+    % Calculate the mean (mu) and variance of the coefficients (s) given
+    % that the coefficients are included in the model, and the posterior
+    % inclusion probabilities (alpha), ignoring correlations between
+    % markers.
+    [alpha(:,i) mu(:,i) s(:,i)] = indbvs(xy,d,sigma(i),sa(i),...
+                                         log(10)*theta0(i));
+  end
+
+  % Compute the posterior expectations averaged over the settings of the
+  % hyperparameters.
+  alpha = alpha * w(:);
+  mu    = mu    * w(:);
+  s     = s     * w(:);
