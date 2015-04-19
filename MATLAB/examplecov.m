c24049a5bc9532dae4f7f2d81df50ca877a47a6d
Peter Carbonetto <pcarbo@uchicago.edu>
2015-04-19 10:20:23 -0700

    Added comments to the top of examplecov.m, and made a few small adjustments to the script.

--- MATLAB/examplecov.m
+++ MATLAB/examplecov.m
@@ -1,15 +1,29 @@
-% EXPLAIN HERE WHAT THIS SCRIPT DOES.
+% This script ilustrates the use of Bayesian variable selection model
+% when:
 %
-%   Illustrates use of Bayesian variable selection model when...
+%   (1) The outcome (Y) is a quantitative trait;
 %
-%   * The outcome (Y) is a quantitative trait.
+%   (2) We have a uniform prior for the proportion of variance
+%       explained (h);
 %
-%   * We have a uniform prior for the proportion of variance explained.
+%   (3) We have a uniform prior for the prior log-odds of
+%       inclusion (theta0).
 %
-%   * We have a uniform prior for the prior log-odds of inclusion.
+%   (4) We have additional variables included in the model with
+%       probability 1 (these are the covariates).
 %
-%   * We have additional variables included in the model with probability
-%    (these are the covariates).
+% After the computation of the variational approximation has completed, I
+% summarize the results of the posterior inference:
+%
+%   (1) The posterior means and 90% credible intervals of the three
+%       hyperparameters (see cred.m for a definition of the credible
+%       interval);
+%
+%   (2) The variables that are selected from the model with posterior
+%       probability at least 10%, ordered by posterior inclusion probability
+%       (PIP), along with their PIPs, mean regression coefficients given
+%       that they are included in the model (mu) and the ground-truth
+%       regression coefficient (beta).
 %
 % This script has been tested in MATLAB R2014b (8.4).
 %
@@ -18,16 +32,16 @@ clear
 % SCRIPT PARAMETERS
 % -----------------
 n  = 500;  % Number of samples.
-p  = 2e3;  % Number of variables (markers).
-m  = 3;    % Number of covariates (variables included with probability 1).
-na = 20;   % Number of QTLs.
-r  = 0.5;  % Proportion of variance in residuals explained by QTLs.
+p  = 2e3;  % Number of variables (genetic markers).
+m  = 3;    % Number of covariates (0 is allowed).
+na = 20;   % Number of quantitative trait loci (QTLs).
 se = 4;    % Variance of residual.
+r  = 0.5;  % Proportion of variance in trait explained by QTLs.
 
 % Candidate values for the prior proportion of variance explained (h) and
-% prior log-odds of inclusion (theta0).
-%
-% TO DO: Explain how these candidate parameter settings were chosen.
+% prior log-odds of inclusion (theta0). These candidate settings were
+% chosen after some trial and error so that they adequately cover the
+% posterior mass.
 theta0 = (-3:0.1:-1.5)';
 h      = (0.2:0.05:0.8)';
 
@@ -59,7 +73,8 @@ beta(I) = randn(na,1);
 sb   = r/(1-r)/var(X*beta,1);
 beta = sqrt(sb*se) * beta;
 
-% Generate the covariate data, and the linear effects of the covariates.
+% Generate the covariate data (Z), and the linear effects of the
+% covariates (u).
 if m > 0
   Z = randn(n,m);
   u = randn(m,1);
@@ -94,9 +109,10 @@ X = X - Z*((Z'*Z)\(Z'*X));
 
 % COMPUTE VARIATIONAL APPROXIMATION
 % ---------------------------------
-fprintf('Computing variational estimates.\n');
+fprintf('Computing variational approximation.\n');
 [H THETA0] = ndgrid(h,theta0);
 [logw sigma alpha mu s] = multisnphyper(X,y,H,THETA0,false);
+fprintf('\n');
 
 % Compute the normalized importance weights.
 w = normalizelogweights(logw);
@@ -108,8 +124,8 @@ w = normalizelogweights(logw);
 fprintf('Posterior distribution of hyperparameters:\n');
 fprintf('param    mean Pr>90%%\n');
 
-% Show the posterior mean and "credible interval" for sigma (not a true
-% credible interval).
+% Show the posterior mean and credible interval for sigma (note that this
+% is not a true credible interval).
 [ans I] = sort(sigma(:));
 t     = dot(sigma(:),w(:));
 [a b] = cred(sigma(I),w(I),t,0.9);
@@ -124,7 +140,7 @@ fprintf('theta0 %+0.3f [%+0.1f,%+0.1f]\n',t,a,b);
 t     = dot(H(:),w(:));
 [a b] = cred(h,sum(w,2),t,0.9);
 fprintf('h      %6.3f [%0.2f,%0.2f]\n',t,a,b);
-fprintf('Note: not a true credible interval.\n');
+fprintf('*Note: not a true credible interval.\n');
 fprintf('\n');
 
 % Calculate the posterior inclusion probabilities (PIPs), and show the PIPs
