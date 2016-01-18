c12a3efd8fab49a1c7c657c38e3c1744e6c9bfa6
Peter Carbonetto <peter.carbonetto@gmail.com>
2016-01-17 23:18:11 -0800

    Added appropriate return value to end of print.varbvs.

--- varbvs-R/R/print.varbvs.R
+++ varbvs-R/R/print.varbvs.R
@@ -1,6 +1,6 @@
 # Generate a four-part summary of the fitted Bayesian variable
 # selection model.
-print.varbvs <- function (fit, c = 0.95, n = 5, nr = 1000) {
+print.varbvs <- function (fit, cred.int = 0.95, n = 5, nr = 1000) {
 
   # Get the number of variables (p) and number of candidate
   # hyperparameter settings (ns).
@@ -28,6 +28,19 @@ print.varbvs <- function (fit, c = 0.95, n = 5, nr = 1000) {
   cat(sprintf("variables:  %-6d",p))
   cat(sprintf("     fit prior var. of coefs (sa): %s\n",tf2yn(fit$update.sa)))
   cat(sprintf("covariates: %-6d     ",fit$ncov))
- 
-  return(NULL)
+  if (fit$family == "gaussian")
+    cat(sprintf("fit residual var. (sigma):    %s\n",tf2yn(fit$update.sigma)))
+  else if (fit$family == "binomial")
+    cat(sprintf("fit approx. factors (eta):    %s\n",tf2yn(fit$optimize.eta)))
+  cat(sprintf("maximum log-likelihood lower bound: %0.4f\n",max(fit$logw)))
+  if (fit$family == "gaussian") {
+    x  <- sort(fit$model.pve)
+    x0 <- mean(x)
+    a  <- x[floor((0.5 - cred.int/2)*length(x))]
+    b  <- x[ceiling((0.5 + cred.int/2)*length(x))]
+    cat("proportion of variance explained: ")
+    cat(sprintf("%0.1f%% [%0.1f%%,%0.1f%%]\n",100*x0,100*a,100*b))
+  }
+  
+  return(invisible(fit))
 }
