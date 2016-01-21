f37cae1c3b6694fe3aa40474c6896b6fbe70112f
Peter Carbonetto <peter.carbonetto@gmail.com>
2016-01-21 07:33:12 -0800

    Reorganized and removed some files.

--- unused-files/ndsum.m
+++
@@ -1,3 +0,0 @@
-% NDSUM(X,DIMS) returns NDOP(@SUM,X,DIMS).
-function x = ndsum (x, dims)
-  x = ndop(@sum,x,dims);
--- unused-files/relerr.m
+++
@@ -1,9 +0,0 @@
-% relerr(x,y) returns the absolute relative error between x and y. Note that
-% relerr(x,y) = relerr(y,x), and we define relerr([],[]) = 0.
-function y = relerr (x, y)
-  if isempty(x) | isempty(y)
-    y = 0;
-  else
-    y = abs(x-y)./(abs(x) + abs(y) + eps);
-  end
-  
