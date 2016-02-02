13945d51d8f9825a2f43cbefded485f926920eba
Peter Carbonetto <peter.carbonetto@gmail.com>
2016-02-02 00:44:26 -0600

    Started working on R script demo.cd.R.

---
+++ demo.cd.R
@@ -0,0 +1,44 @@
+# This script fits the Bayesian variable selection model to identify genetic
+# markers associated with Crohn's disease risk. The data consist of 442,001
+# SNPs genotyped for 1,748 cases and 2,938 controls.
+library(varbvs)
+
+# Initialize the random number generator. 
+set.seed(1)
+
+# LOAD GENOTYPE AND PHENOTYPE DATA
+# --------------------------------
+cat("LOADING DATA.\n")
+load("~/data/cd.RData")
+
+# FIT VARIATIONAL APPROXIMATION TO POSTERIOR
+# ------------------------------------------
+# Fit the fully-factorized variational approximation to the posterior
+# distribution of the coefficients for a logistic regression model of
+# a binary outcome (case-control status), with spike and slab priors
+# on the coefficients.
+cat("FITTING MODEL TO DATA.\n")
+fit <- varbvs(X,NULL,y,"binomial",logodds = seq(-6,-3,0.25))
+
+# SUMMARIZE POSTERIOR DISTRIBUTION
+# --------------------------------
+cat("SUMMARIZING RESULTS.\n")
+varbvsprint(fit,n = 9)
+
+# Compute "single-marker" posterior inclusion probabilities.
+w <- c(normalizelogweights(fit$logw))
+# TO DO.
+
+# Show two "genome-wide scans", one using the posterior inclusion
+# probabilities (PIPs) computed in the joint analysis of all
+# variables, and one using the PIPs that ignore correlations between
+# the variables. The latter is meant to look like a typical
+# genome-wide "Manhattan" plot used to summarize the results of a
+# genome-wide association study. Variables with PIP > 0.5 are
+# highlighted.
+# TO DO.
+
+# SAVE RESULTS
+# ------------
+cat("SAVING RESULTS.\n")
+# TO DO.
