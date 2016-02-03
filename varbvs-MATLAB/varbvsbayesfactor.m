d6c306a92edbc1d28c257229d54539b9ac190559
Peter Carbonetto <peter.carbonetto@gmail.com>
2016-02-03 09:59:53 -0600

    I have a first draft of varbvsbayesfactor.m.

--- varbvs-MATLAB/varbvsbayesfactor.m
+++ varbvs-MATLAB/varbvsbayesfactor.m
@@ -1,13 +1,15 @@
+% TO DO: Describe this function here.
+function BF = varbvsbayesfactor (logw0, logw1)
 
-% Compute the marginal log-likelihood under the null hypothesis using
-% importance sampling.
-c     = max(logw0(:));
-logZ0 = c + log(mean(exp(logw0(:) - c)));
+  % Compute the marginal log-likelihood under the null hypothesis using
+  % importance sampling.
+  c     = max(logw0(:));
+  logz0 = c + log(mean(exp(logw0(:) - c)));
 
-% Compute the marginal log-likelihood under the enrichment hypothesis using
-% importance sampling.
-c     = max(logw1(:));
-logZ1 = c + log(mean(exp(logw1(:) - c)));
+  % Compute the marginal log-likelihood under the alternative hypothesis
+  % using importance sampling.
+  c     = max(logw1(:));
+  logz1 = c + log(mean(exp(logw1(:) - c)));
 
-  % Get the numerical estimate of the Bayes factor.
-  BF = exp(logZ1 - logZ0);
+  % Compute the numerical estimate of the Bayes factor.
+  BF = exp(logz1 - logz0);
