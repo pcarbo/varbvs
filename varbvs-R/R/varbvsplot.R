2ea909822ff8165206f172ea609a90b570628ca0
Peter Carbonetto <peter.carbonetto@gmail.com>
2016-02-03 23:01:29 -0800

    I have a first draft of function 'varbvsplot' in R.

--- varbvs-R/R/varbvsplot.R
+++ varbvs-R/R/varbvsplot.R
@@ -1,24 +1,24 @@
 # Summarize the variable selection results in a single plot.
 varbvsplot <- function (fit, score = NULL, vars = NULL, groups = NULL,
-                        gaps = 0) {
-
+                        gap = 0, col = "midnightblue",col.vars = "magenta",
+                        xlab = "", ylab = "",...) {
+  
   # PROCESS OPTIONS
   # ---------------
   # Calculate the posterior inclusion probabilities (PIPs) if a
   # "score" isn't provided as one of the inputs.
-  if (is.null(score))
-    y <- score
-  else {
+  if (is.null(score)) {
     w <- c(normalizelogweights(fit$logw))
     y <- fit$alpha %*% w
-  }
-  p <- numel(y);
+  } else
+    y <- score
+  p <- length(y)
 
   # Determine the grouping of the variables. By default, all the
   # variables are assigned to a single group.
   if (is.null(groups))
     groups <- rep(1,p)
-  group_labels <- unique(groups)
+  group.labels <- unique(groups)
 
   # GENERATE GENOME-WIDE SCAN PLOT
   # ------------------------------
@@ -26,7 +26,7 @@ varbvsplot <- function (fit, score = NULL, vars = NULL, groups = NULL,
   x      <- rep(0,p)
   pos    <- 0
   xticks <- NULL
-  for (i in group_labels) {
+  for (i in group.labels) {
     j      <- which(groups == i)
     m      <- length(j)
     x[j]   <- pos + 1:m
@@ -36,5 +36,15 @@ varbvsplot <- function (fit, score = NULL, vars = NULL, groups = NULL,
 
   # Plot the posterior probabilities, highlighting and labeling the
   # selected variables.
-  return(xyplot(y ~ x,data.frame(x = x,y = y),pch = 20))
+  labels <- rownames(fit$alpha)
+  return(xyplot(y ~ x,data.frame(x = x,y = y),pch = 20,col = col,
+                scales = list(x = list(at = xticks,labels = group.labels)),
+                xlab = xlab,ylab = ylab,...) +
+         as.layer(xyplot(y ~ x,data.frame(x = x,y = y)[vars,],pch = 20,
+                         col = col.vars,
+                         panel = function (x,y,...) {
+                           panel.xyplot(x,y,...);
+                           ltext(x = x,y = y,labels = labels[vars],
+                                 pos = 4,cex = 0.5)
+                         })))
 }
