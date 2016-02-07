dfb334cecdd1dceaf6b172179421b21d2a67bae8
Peter Carbonetto <peter.carbonetto@gmail.com>
2016-02-07 10:04:36 -0800

    Implemented function 'bayesfactor' in R.

---
+++ varbvs-R/R/bayesfactor.R
@@ -0,0 +1,14 @@
+# Compute numerical estimate of Bayes factor.
+varbvsbayesfactor <- function (logw0, logw1) {
+
+  # Compute the marginal log-likelihood under the null hypothesis.
+  c     <- max(logw0)
+  logz0 <- c + log(mean(exp(logw0 - c)))
+
+  # Compute the marginal log-likelihood under the alternative hypothesis.
+  c     <- max(logw1)
+  logz1 <- c + log(mean(exp(logw1 - c)))
+
+  # Compute the numerical estimate of the Bayes factor.
+  return(exp(logz1 - logz0))
+}
