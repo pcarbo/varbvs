d8e6969ca7c37d360642ea8105a53dd821fd2354
Peter Carbonetto <peter.carbonetto@gmail.com>
2016-02-12 22:36:46 -0800

    Added importFrom statements for plot.varbvs.R.

--- varbvs-R/R/plot.varbvs.R
+++ varbvs-R/R/plot.varbvs.R
@@ -1,30 +1,35 @@
 # Summarize the variable selection results in a single plot.
-plot.varbvs <- function (fit, score = NULL, groups = NULL, gap = 0,
-                         col = "midnightblue", vars = NULL, var.labels = NULL,
-                         var.col = "magenta", pch = 20, score.line = NULL,
+plot.varbvs <- function (fit, score, groups, vars, var.labels, gap = 0,
+                         col = "midnightblue", var.col = "magenta", pch = 20,
                          ltext.args = "col=\"black\",pos=4,cex=0.5",
-                         xlab = "", ylab = "",...) {
+                         score.line, xlab = "", ylab = "", ...) {
+
+  # CHECK INPUTS
+  # ------------
+  # Check that the first input is an instance of class "varbvs".
+  if (!is(fit,"varbvs"))
+    stop("Input fit must be an instance of class \"varbvs\".")
   
   # PROCESS OPTIONS
   # ---------------
   # Calculate the posterior inclusion probabilities (PIPs) if a
   # "score" isn't provided as one of the inputs.
-  if (is.null(score)) {
+  if (missing(score)) {
     w <- c(normalizelogweights(fit$logw))
     y <- fit$alpha %*% w
   } else
     y <- score
   p <- length(y)
 
-  # Determine the grouping of the variables. By default, all the
-  # variables are assigned to a single group.
-  if (is.null(groups))
+  # Determine the grouping of the variables. By default, all variables
+  # are assigned to one group.
+  if (missing(groups))
     groups <- rep(1,p)
   group.labels <- unique(groups)
 
   # Determine the selected variable labels. By default, use the labels
   # stored in the varbvs data structure ("fit").
-  if (is.null(var.labels))
+  if (missing(var.labels))
     var.labels <- rownames(fit$alpha)[vars]
   
   # GENERATE GENOME-WIDE SCAN PLOT
@@ -54,7 +59,7 @@ plot.varbvs <- function (fit, score = NULL, groups = NULL, gap = 0,
                 scales = list(x = list(at = xticks,labels = group.labels)),
                 panel = function (x,y,...) {
                   panel.xyplot(x,y,...);
-                  if (!is.null(score.line))
+                  if (!missing(score.line))
                     panel.abline(a = score.line,b = 0,lty = "dotted",
                                  col = "orangered")
                 },
