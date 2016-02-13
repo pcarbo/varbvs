d459368a2acb421d3bf3dd531de3d1ae63e99fdf
Peter Carbonetto <peter.carbonetto@gmail.com>
2016-02-12 21:55:51 -0800

    Added steps to check inputs X, Z and y in R function 'varbvs'.

--- varbvs-R/R/varbvs.R
+++ varbvs-R/R/varbvs.R
@@ -13,6 +13,10 @@ varbvs <- function (X, Z, y, family = "gaussian", sigma = NULL, sa = NULL,
 
   # (1) CHECK INPUTS
   # ----------------
+  # Check input X.
+  if (!(is.matrix(X) & is.double(X) & is.na(X) == 0))
+    stop("Input X must be a double-precision matrix with no missing values.")
+  
   # Add column names to X if they are not provided.
   if (is.null(colnames(X)))
     colnames(X) <- paste0("X",1:p)
@@ -20,8 +24,11 @@ varbvs <- function (X, Z, y, family = "gaussian", sigma = NULL, sa = NULL,
   # Check input Z.
   if (!is.null(Z)) {
     Z <- as.matrix(Z)
+    if (!is.numeric(Z) | is.na(Z) > 0)
+      stop("Input Z must be a numeric matrix with no missing values.")
     if (nrow(Z) != n)
-      stop("Inputs X and Z do not match")
+      stop("Inputs X and Z do not match.")
+    storage.mode(Z) <- "double"
   }
   
   # Add intercept.
@@ -30,7 +37,9 @@ varbvs <- function (X, Z, y, family = "gaussian", sigma = NULL, sa = NULL,
   else
     Z <- cbind(1,Z)
 
-  # Input y must have n entries.
+  # Check input y.
+  if (!is.numeric(y) | is.na(y) > 0)
+    stop("Input y must be a numeric vector with no missing values.")
   if (length(y) != n)
     stop("Inputs X and y do not match")
   y <- c(as.double(y))
