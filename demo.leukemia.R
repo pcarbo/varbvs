8f05889c5bdead17b60e9de2df297a80c14cfe72
Peter Carbonetto <peter.carbonetto@gmail.com>
2016-02-21 16:00:00 -0800

    Implemented analysis of leukemia data with glmnet in demo.leukemia.R.

--- demo.leukemia.R
+++ demo.leukemia.R
@@ -8,64 +8,98 @@ library(varbvs)
 
 # SCRIPT PARAMETERS
 # -----------------
-# TO DO.
-
+nfolds <- 20                  # Number of cross-validation folds.
+alpha  <- 0.95                # Elastic net mixing parameter.
+lambda <- 10^(seq(-2,0,0.05)) # Lambda sequence.
+              
 # LOAD LEUKEMIA DATA
 # ------------------
 cat("1. Loading leukemia data.\n")
 load("varbvs-R/data/leukemia.RData")
 X <- Leukemia$x
 y <- Leukemia$y
+rm(Leukemia)
 
 # Set the random number generator seed.
 set.seed(1)
 
 # FIT ELASTIC NET MODEL TO DATA
 # -----------------------------
+# First run 20-fold cross-validation to select the largest setting of
+# lambda that is within one standard error of the minimum
+# classification error.
 cat("2. Running 20-fold cross-validation to select L1-penalty strength.\n")
-out <- cv.glmnet(X,y,family = "binomial",type.measure = "class",alpha = 0.2,
-                 nfolds = 20,lambda = 10^(seq(-1,0.5,0.05)))
+out.cv.glmnet <-
+  cv.glmnet(X,y,family = "binomial",type.measure = "class",alpha = alpha,
+            nfolds = nfolds,lambda = lambda)
 
-# Show the misclassification error at different settings of the
-# L1-penalty strength. Then choose the largest value of the
-# L1-penalty strength (lambda) that is within 1 standard error of the
-# smallest misclassification error.
-plot(out)
-lambda <- out$lambda.1se
+# Show the classification error at different settings of lambda. Then
+# choose the largest value of lambda that is within 1 standard error
+# of the smallest misclassification error.
+lambda.opt <- out.cv.glmnet$lambda.1se
 
-trellis.device(height = 4,width = 5)
-print(xyplot(y ~ x,data.frame(x = log10(out$lambda),y = out$cvm),type = "l",
-             col = "blue",xlab = "log10 lambda",ylab = "error",
-             panel = function(x, y, ...) {
-               panel.xyplot(x,y,...)
-               panel.abline(v = log10(out$lambda.1se),col = "orangered",
-                            lwd = 1,lty = "dotted")
-             }, 
-             scales = list(y = list(limits = c(-0.02,0.45)))) +
-      as.layer(xyplot(y ~ x,data.frame(x = log10(out$lambda),y = out$cvm),
-                      pch = 20,col = "blue")) +
-      as.layer(xyplot(y ~ x,data.frame(x = log10(out$lambda),y = out$cvup),
-                      type = "l",col = "skyblue")) +
-      as.layer(xyplot(y ~ x,data.frame(x = log10(out$lambda),y = out$cvlo),
-                      type = "l",col = "skyblue")),
-      split = c(1,1,1,2),more = TRUE)
+# Fit the elastic net model to the data.
+cat("3. Fitting elastic net model.\n")
+fit.glmnet <- glmnet(X,y,family = "binomial",lambda = lambda,alpha = alpha)
 
-print(xyplot(y ~ x,data.frame(x = log10(out$lambda),y = out$nzero),type = "l",
+# Plot the classification error at different settings of lambda.
+cat("4. Summarizing results of glmnet analysis.\n")
+trellis.device(height = 4.5,width = 7)
+trellis.par.set(par.xlab.text = list(cex = 0.65),
+                par.ylab.text = list(cex = 0.65),
+                axis.text     = list(cex = 0.65))
+print(with(out.cv.glmnet,
+           xyplot(y ~ x,data.frame(x = log10(lambda),y = cvm),type = "l",
+                  col = "blue",xlab = "log10 lambda",ylab = "error",
+                  scales = list(y = list(limits = c(-0.02,0.45))),
+                  panel = function(x, y, ...) {
+                    panel.xyplot(x,y,...)
+                    panel.abline(v = log10(lambda.opt),col = "orangered",
+                                 lwd = 1,lty = "dotted")
+                  }) +
+           as.layer(xyplot(y ~ x,data.frame(x = log10(lambda),y = cvm),
+                           pch = 20,cex = 0.6,col = "blue")) +
+           as.layer(xyplot(y ~ x,data.frame(x = log10(lambda),y = cvup),
+                           type = "l",col = "blue",lty = "dashed")) +
+           as.layer(xyplot(y ~ x,data.frame(x = log10(lambda),y = cvlo),
+                           type = "l",col = "blue",lty = "dashed"))),
+           split = c(1,1,2,2),more = TRUE)
+
+# Show the number of nonzero regression coefficients at different
+# settings of lambda.
+print(with(out.cv.glmnet,
+           xyplot(y ~ x,data.frame(x = log10(lambda),y = nzero),type = "l",
              col = "blue",xlab = "log10 lambda",ylab = "num. nonzero coefs",
              panel = function(x, y, ...) {
                panel.xyplot(x,y,...)
-               panel.abline(v = log10(out$lambda.1se),col = "orangered",
+               panel.abline(v = log10(lambda.opt),col = "orangered",
                             lwd = 1,lty = "dotted")
-             }),
-      split = c(1,2,1,2),more = FALSE)
+             }) +
+      as.layer(xyplot(y ~ x,data.frame(x = log10(lambda),y = nzero),
+                      pch = 20,cex = 0.6,col = "blue"))),
+      split = c(1,2,2,2),more = TRUE)
+
+# Plot evolution of regression coefficients at different settings of
+# lambda. Do not show the intercept.
+vars <- setdiff(which(rowSums(abs(coef(fit.glmnet))) > 0),1)
+n    <- length(vars)
+b    <- as.matrix(t(coef(fit.glmnet)[vars,]))
+r    <- xyplot(y ~ x,data.frame(x = log10(lambda),y = b[,1]),type = "l",
+               col = "blue",scales = list(y = list(limits = c(-0.8,1.2))),
+               xlab = "log10 lambda",ylab = "regression coefficient",
+               panel = function(x, y, ...) {
+                 panel.xyplot(x,y,...)
+                 panel.abline(v = log10(lambda.opt),col = "orangered",
+                              lwd = 1,lty = "dotted")
+               })
+for (i in 2:n)
+  r <- r + as.layer(xyplot(y ~ x,
+                           data.frame(x = log10(lambda),y = b[,i]),type = "l",
+                           col = "blue"))
+print(r,split = c(2,1,2,1),more = FALSE)
+rm(vars,n,b,r,i)
 
 stop()
-
-# Fit the elastic net model to the data.
-cat(sprintf("3. Fitting elastic net model with lambda = %0.3f.\n",lambda))
-fit.glmnet <- glmnet(X,y,family = "binomial",alpha = 0.2)
-dev.new(height = 3,width = 5)
-plot(fit.glmnet,xvar = "lambda",xlim = c(-2,1))
 
 # FIT VARIATIONAL APPROXIMATION TO POSTERIOR
 # ------------------------------------------
