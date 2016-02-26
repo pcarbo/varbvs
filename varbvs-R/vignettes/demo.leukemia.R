dafbe6e157d4c2d8661709c7aae7d53c60ae4022
Peter Carbonetto <peter.carbonetto@gmail.com>
2016-02-26 07:10:38 -0800

    Made some changes to the way I plot the results of varbvs in demo.leukemia.R.

--- varbvs-R/vignettes/demo.leukemia.R
+++ varbvs-R/vignettes/demo.leukemia.R
@@ -15,6 +15,7 @@ lambda <- 10^(seq(-2,0,0.05)) # Lambda sequence.
 
 # varbvs settings.
 logodds <- seq(-3.5,-1.5,0.1)  # Candidate prior log-odds settings.
+sa      <- 1                   # Prior variance of coefficients.
 
 # LOAD LEUKEMIA DATA
 # ------------------
@@ -122,7 +123,8 @@ rm(vars,n,b,r,i)
 # the binary outcome (type of leukemia), with spike-and-slab priors
 # on the coefficients.
 cat("5. Fitting Bayesian variable selection model to data.\n")
-fit.varbvs <- varbvs(X,NULL,y,"binomial",logodds = logodds,verbose = FALSE)
+fit.varbvs <- varbvs(X,NULL,y,"binomial",logodds = logodds,sa = sa,
+                     verbose = FALSE)
 
 # Compute estimates of the disease outcome using the fitted model, and
 # compare against the observed values.
@@ -131,35 +133,11 @@ cat("classification results from fitted varbvs model:\n")
 y.varbvs <- predict(fit.varbvs,X)
 print(table(true = factor(y),pred = factor(y.varbvs)))
 
-# Plot evolution of posterior inclusion probabilities (PIPs) at
-# different settings of the prior log-odds.
-trellis.device(height = 6,width = 3.5)
+# Show the classification error at each setting of the prior log-odds.
+trellis.device(height = 4.5,width = 7)
 trellis.par.set(par.xlab.text = list(cex = 0.65),
                 par.ylab.text = list(cex = 0.65),
                 axis.text     = list(cex = 0.65))
-m     <- length(logodds)
-n     <- 10
-vars  <- order(fit.varbvs$alpha[,m],decreasing = TRUE)[1:n]
-alpha <- t(fit.varbvs$alpha[vars,])
-r     <- xyplot(y ~ x,data.frame(x = logodds,y = log10(alpha[,1])),
-                scales = list(x = list(limits = c(-3.75,-1.4)),
-                              y = list(limits = c(-4.5,0.4))),
-                type = "l",col = "blue",xlab = "prior log-odds",
-                ylab = "log10 PIP",
-                panel = function(x, y, ...) {
-                  panel.xyplot(x,y,...);
-                  ltext(x = -3.5,y = log10(alpha[1,]),
-                        labels = colnames(alpha),pos = 2,
-                        offset = 0.5,cex = 0.5)
-                })
-for (i in 2:n)
-  r <- r + as.layer(xyplot(y ~ x,
-                           data.frame(x = logodds,y = log10(alpha[,i])),
-                           type = "l",col = "blue"))
-print(r,split = c(1,1,1,3),more = TRUE)
-rm(m,n,i,vars,alpha,r)
-
-# Show the classification error at each setting of the prior log-odds.
 m    <- length(logodds)
 err  <- rep(0,m)
 logw <- fit.varbvs$logw
@@ -170,22 +148,48 @@ for (i in 1:m) {
   err[i] <- mean(y != ypred)
 }
 fit.varbvs$logw <- logw
-print(xyplot(y ~ x,data.frame(x = logodds,y = err),type = "l",col = "blue",
-             xlab = "prior log-odds",ylab = "classification error",
-             scales = list(x = list(limits = c(-3.75,-1.4)))) +
-      as.layer(xyplot(y ~ x,data.frame(x = logodds,y = err),col = "blue",
-                      pch = 20,cex = 0.65)),
-      split = c(1,2,1,3),more = TRUE)
-rm(m,err,logw,ypred,i)
+print(xyplot(y ~ x,data.frame(x = logodds,y = err),type = "l",
+             col = "blue",xlab = "prior log-odds",
+             ylab = "classification error",
+             scales = list(x = list(limits = c(-1.4,-3.6)))) +
+      as.layer(xyplot(y ~ x,data.frame(x = logodds,y = err),
+                      col = "blue",pch = 20,cex = 0.65)),
+      split = c(1,1,2,2),more = TRUE)
+rm(err,logw,ypred,i)
 
 # Show probability density of prior log-odds.
 w        <- normalizelogweights(fit.varbvs$logw)
 names(w) <- logodds
 print(xyplot(y ~ x,data.frame(x = logodds,y = w),type = "l",col = "blue",
              xlab = "prior log-odds",ylab = "posterior prob.",
-             scales = list(x = list(limits = c(-3.75,-1.4)))) +
-      as.layer(xyplot(y ~ x,data.frame(x = logodds,y = w),col = "blue",
-               pch = 20,cex = 0.65)),
-      split = c(1,3,1,3),more = FALSE)
+             scales = list(x = list(limits = c(-1.4,-3.6)))) +
+      as.layer(xyplot(y ~ x,data.frame(x = logodds,y = w),
+                      col = "blue",pch = 20,cex = 0.65)),
+      split = c(1,2,2,2),more = TRUE)
 rm(w)
+
+# Plot evolution of posterior inclusion probabilities (PIPs) at
+# different settings of the prior log-odds.
+n     <- 10
+vars  <- order(fit.varbvs$alpha[,m],decreasing = TRUE)[1:n]
+alpha <- t(fit.varbvs$alpha[vars,] * fit.varbvs$mu[vars,])
+alpha <- pmax(alpha,-0.075)
+r     <- xyplot(y ~ x,data.frame(x = logodds,y = (alpha[,1])),
+                type = "l",col = "blue",xlab = "prior log-odds",
+                ylab = "regression coefficient",
+                scales = list(x = list(limits = c(-1.15,-3.6)),
+                              y = list(limits = c(-0.08,0.06))),
+                panel = function(x, y, ...) {
+                  panel.xyplot(x,y,...);
+                  ltext(x = -1.5,y = (alpha[m,]),
+                        labels = colnames(alpha),pos = 2,
+                        offset = 0.5,cex = 0.5)
+                })
+for (i in 2:n)
+  r <- r + as.layer(xyplot(y ~ x,
+                           data.frame(x = logodds,y = (alpha[,i])),
+                           type = "l",col = "blue"))
+print(r,split = c(2,1,2,1),more = FALSE)
+rm(m,n,i,vars,alpha,r)
+
 
