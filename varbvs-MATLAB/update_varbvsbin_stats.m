ee5a1ff864623dbfa855be0102e8ea1d6078c84f
Peter Carbonetto <peter.carbonetto@gmail.com>
2016-04-19 14:20:18 -0700

    Added "unstable" update to update_varbvsbin_stats.m.

--- varbvs-MATLAB/update_varbvsbin_stats.m
+++ varbvs-MATLAB/update_varbvsbin_stats.m
@@ -17,8 +17,13 @@ function stats = update_varbvsbin_stats (X, y, eta)
 
   % Compute the diagonal entries of X'*dhat*X. For a definition of dhat, see
   % the Bayesian Analysis journal paper.
+  %
+  % This is the less numerically stable version of this update:
+  % 
+  %   xdx = diagsq(X,d) - xd.^2/sum(d)
+  % 
   dzr = d/sqrt(sum(d));
   xdx = diagsq(X,d) - double((dzr'*X).^2)';
-
+ 
   % Return the result.
   stats = struct('d',d,'yhat',yhat,'xy',xy,'xd',xd,'xdx',xdx);
