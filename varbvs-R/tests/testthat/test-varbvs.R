5e83c690111f3860ae42e1caa61c272c2cd9d1e0
Peter Carbonetto <peter.carbonetto@gmail.com>
2016-07-24 09:41:17 -0700

    Added more expect_equal statements to test-varbvs.R.

--- varbvs-R/tests/testthat/test-varbvs.R
+++ varbvs-R/tests/testthat/test-varbvs.R
@@ -1,9 +1,23 @@
 context("varbvs")
 
-test_that("model fitting works for simulated data with continuous outcome",{
+test_that("model fitting works for simulated data with a continuous outcome",{
+
+  # Run the R script that demonstrates varbvs for genome-wide mapping
+  # of a quantitative trait in a simulated data set.
   source("demo.qtl.R")
+
+  # Check the number of included variables at different probability
+  # thresholds.
   expect_equal(summary(fit)$num.included,
                as.table(unlist(list(">0.10" = 18,">0.25" = 12,">0.50" = 10,
                                     ">0.75" = 10,">0.90" = 10,">0.95" = 10))))
+
+  # Check the hyperparameter setting with the greatest likelihood value.
+  expect_equal(which.max(summary(fit)$logw),10)
+  
+  # Check the posterior mean of the hyperparameters.
+  expect_equal(summary(fit)$logodds$x0,-2.08,tolerance = 0.01)
+  expect_equal(summary(fit)$sigma$x0,4.13,tolerance = 0.01)
+  expect_equal(summary(fit)$sa$x0,0.158,tolerance = 0.001)
 })
 
