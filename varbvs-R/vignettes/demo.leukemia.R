f60bca9c18458104c56046ce45a54d9753e49bae
Peter Carbonetto <peter.carbonetto@gmail.com>
2016-08-06 08:19:45 -0700

    Revised plots for varbvs analysis of leukemia data.

--- varbvs-R/vignettes/demo.leukemia.R
+++ varbvs-R/vignettes/demo.leukemia.R
@@ -171,6 +171,11 @@ r <- system.time(fit.varbvs <-
 cat(sprintf("Model fitting took %0.2f seconds.\n",r["elapsed"]))
 rm(r)
 
+# Compute the normalized importance weights, and the posterior
+# inclusion probabilities averaged over the hyperparameter settings.
+w <- normalizelogweights(fit.varbvs$logw)
+pip   <- c(fit.varbvs$alpha %*% w)
+
 # Compute estimates of the disease outcome using the fitted model, and
 # compare against the observed values.
 cat("6. Summarizing results of varbvs analysis.\n")
@@ -197,37 +202,54 @@ print(xyplot(y ~ x,data.frame(x = logodds,y = err),type = "l",
       as.layer(xyplot(y ~ x,data.frame(x = logodds,y = err),
                       col = "blue",pch = 20,cex = 0.65)),
       split = c(1,1,2,2),more = TRUE)
-rm(err,ypred,i,r)
+rm(err,ypred,i,m,r)
 
-# Plot evolution of posterior inclusion probabilities (PIPs) at
-# different settings of the prior log-odds.
+# Plot the evolution of the posterior mean regression coefficients
+# (the beta's) at different settings of the prior log-odds, for the
+# top 6 variables ranked by posterior inclusion probability (averaged
+# over settings of the hyperparameters).
 #
-# TO DO: Fix this plot.
-#
-n     <- 10
-vars  <- order(fit.varbvs$alpha[,m],decreasing = TRUE)[1:n]
-alpha <- t(fit.varbvs$alpha[vars,] * fit.varbvs$mu[vars,])
-alpha <- pmax(alpha,-0.075)
-r     <- xyplot(y ~ x,data.frame(x = logodds,y = (alpha[,1])),
-                type = "l",col = "blue",xlab = "prior log-odds",
-                ylab = "regression coefficient",
-                scales = list(x = list(limits = c(-1.15,-3.6)),
-                              y = list(limits = c(-0.08,0.06))),
-                panel = function(x, y, ...) {
-                  panel.xyplot(x,y,...);
-                  ltext(x = -1.5,y = (alpha[m,]),
-                        labels = colnames(alpha),pos = 2,
-                        offset = 0.5,cex = 0.5)
-                })
-for (i in 2:n)
+# The top-ranked variable (by posterior inclusion probability) has a
+# much larger coefficient than all the others, so it is shown in a
+# separate plot.
+n     <- 4
+m     <- length(logodds)
+vars  <- order(pip,decreasing = TRUE)[1:n]
+mu    <- t(fit.varbvs$alpha[vars,] * fit.varbvs$mu[vars,])
+print(xyplot(y ~ x,data.frame(x = logodds,y = (mu[,1])),
+             type = "l",col = "blue",xlab = "prior log-odds",
+             ylab = "regression coefficient",
+             scales = list(x = list(limits = c(-1.15,-3.6)),
+                           y = list(limits=c(-3,-2.5),at=seq(-3,-2.5,0.25))),
+             panel = function(x, y, ...) {
+               panel.xyplot(x,y,...);
+               ltext(x = -1.5,y = mu[m,1],
+                     labels = vars[1],pos = 2,
+                     offset = 0.5,cex = 0.5)
+               }),
+      split = c(2,2,2,2),more = TRUE)
+
+r    <- xyplot(y ~ x,data.frame(x = logodds,y = (mu[,2])),
+               type = "l",col = "blue",xlab = "prior log-odds",
+               ylab = "regression coefficient",
+               scales = list(x = list(limits = c(-1.15,-3.6)),
+                             y = list(limits = c(-0.06,0.06),
+                                      at = c(-0.06,0,0.06))),
+               panel = function(x, y, ...) {
+                 panel.xyplot(x,y,...);
+                 ltext(x = -1.5,y = mu[m,],
+                       labels = vars,pos = 2,
+                       offset = 0.5,cex = 0.5)
+               })
+for (i in 3:n) {
   r <- r + as.layer(xyplot(y ~ x,
-                           data.frame(x = logodds,y = (alpha[,i])),
+                           data.frame(x = logodds,y = mu[,i]),
                            type = "l",col = "blue"))
-print(r,split = c(2,1,2,1),more = TRUE)
-rm(m,n,i,vars,alpha,r)
+}
+print(r,split = c(2,1,2,2),more = FALSE)
+rm(m,n,i,vars,pip,mu,r)
 
 # Show probability density of prior log-odds.
-w        <- normalizelogweights(fit.varbvs$logw)
 names(w) <- logodds
 print(xyplot(y ~ x,data.frame(x = logodds,y = w),type = "l",col = "blue",
              xlab = "prior log-odds",ylab = "posterior prob.",
@@ -235,5 +257,5 @@ print(xyplot(y ~ x,data.frame(x = logodds,y = w),type = "l",col = "blue",
       as.layer(xyplot(y ~ x,data.frame(x = logodds,y = w),
                       col = "blue",pch = 20,cex = 0.65)),
       split = c(1,2,2,2),more = FALSE)
-rm(w)
+
 
