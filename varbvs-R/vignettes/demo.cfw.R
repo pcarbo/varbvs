2e513e56e7c2eb705ddbdd012af21ffb677c96ff
Peter Carbonetto <peter.carbonetto@gmail.com>
2016-08-28 12:06:34 -0700

    Added BVSR results to plots in demo.cfw.R.

--- varbvs-R/vignettes/demo.cfw.R
+++ varbvs-R/vignettes/demo.cfw.R
@@ -1,8 +1,8 @@
 # Map QTLs for phenotypes measured in CFW (Carworth Farms White)
-# outbred mice. Phenotypes include muscle weights (EDL and soleus
-# muscle) and testis weight measured at sacrifice. Running this script
-# with trait = "testis" reproduces the results and figures shown in
-# the second example of Carbonetto et al (2016).
+# outbred mice. Phenotypes include muscle weights---EDL and soleus
+# muscle---and testis weight measured at sacrifice. Running this
+# script with trait = "testis" reproduces the results and figures
+# shown in the second example of Carbonetto et al (2016).
 library(varbvs)
 library(lattice)
 
@@ -10,9 +10,7 @@ library(lattice)
 # -----------------
 # These script parameters specify the candidate prior log-odds
 # settings, the prior variance of the coefficients, and which trait to
-# analyze.
-#
-# Set trait to "edl", "soleus" or "testis".
+# analyze. Set trait to "edl", "soleus" or "testis".
 trait   <- "testis"
 logodds <- seq(-5,-3,0.25)
 sa      <- 0.05
@@ -46,33 +44,40 @@ cat("FITTING MODEL TO DATA.\n")
 runtime <- system.time(fit <- varbvs(X,Z,y,sa = sa,logodds = logodds))
 cat(sprintf("Modeling fitting took %0.2f minutes.\n",runtime["elapsed"]/60))
 
-# Compute "single-marker" posterior inclusion probabilities.
-w   <- c(normalizelogweights(fit$logw))
-pip <- c(varbvsindep(fit,X,Z,y)$alpha %*% w)
-
 # SUMMARIZE POSTERIOR DISTRIBUTION
 # --------------------------------
 cat("SUMMARIZING RESULTS.\n")
 print(summary(fit))
 
 # Show three genome-wide scans: (1) one using the posterior inclusion
-# probabilities (PIPs) computed in the joint analysis of all
-# variables; (2) one using the PIPs that ignore correlations between
-# the variables; and (3) and one using the p-values computed using GEMMA.
+# probabilities (PIPs) computed in the BVS analysis of all SNPs; (2)
+# one using the p-values computed using GEMMA; and (3) one using the
+# PIPs computed from the BVSR model in GEMMA.
 trellis.device(height = 5,width = 10)
 trellis.par.set(axis.text = list(cex = 0.65),
                 par.ylab.text = list(cex = 0.7))
-i <- which(fit$alpha %*% w > 0.5)
+w <- c(normalizelogweights(fit$logw))
+j <- which(fit$alpha %*% w > 0.5)
 r <- gwscan.gemma[[trait]]
 r[is.na(r)] <- 0
-print(plot(fit,groups = map$chr,vars = i,gap = 1500,cex = 0.6,
-           ylab = "posterior prob.",vars.xyplot.args = list(cex = 0.6)),
+chromosomes   <- levels(gwscan.bvsr$chr)
+xticks        <- rep(0,length(chromosomes))
+names(xticks) <- chromosomes
+pos           <- 0
+for (i in chromosomes) {
+  n         <- sum(gwscan.bvsr$chr == i)
+  xticks[i] <- pos + n/2
+  pos       <- pos + n + 25
+}
+print(plot(fit,groups = map$chr,vars = j,gap = 1500,cex = 0.6,
+           ylab = "varbvs prob.",vars.xyplot.args = list(cex = 0.6)),
       split = c(1,1,1,3),more = TRUE)
-print(plot(fit,groups = map$chr,score = log10(pip + 0.001),vars = i,
-           cex = 0.6,gap = 1500,ylab = "log10 posterior prob.",
-           vars.xyplot.args = list(cex = 0.6)),
-      split = c(1,2,1,3),more = TRUE)
-print(plot(fit,groups = map$chr,score = r,vars = i,cex = 0.6,gap = 1500,
+print(plot(fit,groups = map$chr,score = r,vars = j,cex = 0.6,gap = 1500,
            draw.threshold = 5.71,ylab = "-log10 p-value",
            vars.xyplot.args = list(cex = 0.6)),
-     split = c(1,3,1,3),more = FALSE)
+     split = c(1,2,1,3),more = TRUE)
+print(xyplot(p1 ~ plot.x,gwscan.bvsr,pch = 20,col = "midnightblue",
+             scales = list(x = list(at = xticks,labels = chromosomes),
+                           y = list(at = c(0,0.5,1))),
+             ylab = "BVSR prob."),
+      split = c(1,3,1,3),more = FALSE)
