aa5cbb23599a9d51dbd13368b55282381640b0f1
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-01-25 16:48:43 -0600

    Removed roxygen docs for varbvsnormupdate_rcpp.
    Former-commit-id: 4fd8fa73446f17313392c250655cd43ce97aec63

--- varbvs-R/src/varbvsnormupdate_rcpp.cpp
+++ varbvs-R/src/varbvsnormupdate_rcpp.cpp
@@ -7,23 +7,8 @@ double sigmoid_rcpp (double x) {
   return(1/(1 + exp(-x)));
 }
 
-//' Variational update of normal varbvs.
-//'
-//' @param X An n by p matrix of predictors. The rows index the observations and the columns index the variables.
-//' @param sigma A positive numeric. The residual variance.
-//' @param sa A positive numeric. The prior variance.
-//' @param logodds A p-vector that contains the prior log-odds of inclusion for each variable.
-//' @param xy A p-vector containing \code{t(X) \%*\% y}, where \code{y} is a response vector.
-//' @param d The diagonal of the Gram matrix, \code{d = diag(t(X) \%*\% X)}
-//' @param alpha A p-vector of inclusion probabilities for each variable.
-//' @param mu A p-vector of means for each coefficient.
-//' @param Xr An n-vector with elements \code{X \%*\% (alpha * mu)}
-//' @param i A p-vector of integers that give the order of updating the coefficients.
-//'
-//' @export
-//'
-//' @author David Gerard
-//'
+// Execute a single coordinate ascent update to maximize the variational 
+// lower bound for Bayesian variable selection in linear regression.
 // [[Rcpp::export]]
 void varbvsnormupdate_rcpp (const NumericMatrix& X, double sigma, double sa,
 			    const NumericVector& logodds,
