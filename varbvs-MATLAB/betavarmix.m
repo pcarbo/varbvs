8ab0e69bcd7c258553aa511b84e392cfa9537d30
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-04-25 12:49:06 -0500

    Checked that varbvsmix is equal to betavar in special case when k=2 and first component is the spike.
    Former-commit-id: 13584dcbdc52aa50e92feb0a2e44baed1b02bbd9

--- varbvs-MATLAB/betavarmix.m
+++ varbvs-MATLAB/betavarmix.m
@@ -1,7 +1,15 @@
 % betavarmix(p,mu,s) returns variances of variables drawn from mixtures of
-% normals. Each of the inputs is a p x k matrix, where p is the number of
+% normals. Each of the inputs is a n x k matrix, where n is the number of
 % variables and k is the number of mixture components. Specifically,
 % variable i is drawn from a mixture in which the jth mixture component is
 % the univariate normal with mean mu(i,j) and variance s(i,j).
+%
+% Note that the following two lines should return the same result when k=2
+% and the first component is the "spike" density with zero mean and
+% variance.
+%
+%   y1 = betavar(p,mu,s)
+%   y2 = betavarmix([1-p p],[zeros(n,1) mu],[zeros(n,1) s])
+%
 function y = betavarmix (p, mu, s)
   y = sum(p.*(s + mu.^2),2) - sum(p.*mu,2).^2;
