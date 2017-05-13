24585c1cf2334adcb2859052519efdc18949a67a
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-05-12 21:57:35 -0500

    Added code to generate data set in demo.mix.R.
    Former-commit-id: e5587396bed79594d64ee18152d52bb3056e5192

--- varbvs-R/tests/testthat/demo.mix.R
+++ varbvs-R/tests/testthat/demo.mix.R
@@ -5,16 +5,64 @@ library(varbvs)
 # SCRIPT PARAMETERS
 # -----------------
 n  <- 1000  # Number of samples.
-m  <- 2     # Number of covariates (m >= 0).
 p  <- 2000  # Number of variables (genetic markers).
 se <- 4     # Variance of residual.
+
+# Names of covariates.
+covariates <- c("age","weight")
 
 # The standard deviations and mixture weights used to simulate the
 # additive effects on the quantitative trait. Note that the first
 # mixture component must have a standard deviation of exactly zero.
-sd <- [    0  0.1  0.2  0.5 ]
-q  <- [ 0.95 0.03 0.01 0.01 ]
+sd <- c(0,   0.1, 0.2, 0.5)
+q  <- c(0.95,0.03,0.01,0.01)
 
 # Set the random number generator seed.
 set.seed(1)
 
+# GENERATE DATA SET
+# -----------------
+# Get the number of covariates.
+m <- length(covariates)
+
+# Generate the minor allele frequencies so that they are uniform over
+# range [0.05,0.5]. Then simulate genotypes assuming all markers are
+# uncorrelated (i.e., unlinked), according to the specified minor
+# allele frequencies.
+cat("1. GENERATING DATA SET.\n")
+cat("Data simulation settings:\n")
+cat(sprintf("  - Num. data samples       %d\n",n))
+cat(sprintf("  - Num. covariates         %d\n",m))
+cat(sprintf("  - Num. variables (SNPs)   %d\n",p))
+cat(sprintf("  - Num. mixture components %d\n",length(q)))
+maf <- 0.05 + 0.45*runif(p)
+X   <- (runif(n*p) < maf) +
+       (runif(n*p) < maf)
+X   <- matrix(as.double(X),n,p,byrow = TRUE)
+
+# Generate additive effects according to the specified standard
+# deviations (sd) and mixture weights (q).
+k    <- sample(length(q),p,replace = TRUE,prob = q)
+beta <- sd[k] * rnorm(p)
+
+# Generate random labels for the markers.
+colnames(X) <- paste0("rs",sample(1e6,p))
+
+# Generate a random intercept.
+mu <- rnorm(1)
+
+# Generate the covariate data (Z), and the linear effects of the
+# covariates (u).
+if (m > 0) {
+  Z <- randn(n,m)
+  u <- rnorm(m)
+  colnames(Z) <- covariates
+} else {
+  Z <- NULL
+}
+
+# Generate the quantitative trait measurements.
+y <- mu + X %*% beta + sqrt(se)*rnorm(n)
+if (m > 0)
+  y <- y + Z %*% u
+y <- c(y)
