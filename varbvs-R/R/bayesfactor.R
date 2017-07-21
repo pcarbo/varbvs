1e41be056b9395350e9015a4b1ef207f2a431e73
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-07-21 04:17:21 -0500

    Implemented function varbvsbf.

--- varbvs-R/R/bayesfactor.R
+++ varbvs-R/R/bayesfactor.R
@@ -25,3 +25,16 @@ bayesfactor <- function (logw0, logw1) {
   # Compute the numerical estimate of the Bayes factor.
   return(exp(logz1 - logz0))
 }
+
+# A more user-friendly interface for computing a Bayes factor
+# comparing two Bayesian variable selection models.
+varbvsbf <- function (fit0, fit1) {
+
+  # Check that the first and second inputs are of class "varbvs".
+  if (!is(fit0,"varbvs"))
+    stop("Argument \"fit0\" should be a varbvs object")
+  if (!is(fit1,"varbvs"))
+    stop("Argument \"fit1\" should be a varbvs object")
+
+  return(bayesfactor(fit0$logw,fit1$logw))
+}
