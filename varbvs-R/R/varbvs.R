80723bea2fd0a26fa5efcaee95a8961da93077ae
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-08-17 10:17:45 -0500

    Bumped up version; R CMD --as-cran returns OK.

--- varbvs-R/R/varbvs.R
+++ varbvs-R/R/varbvs.R
@@ -209,7 +209,7 @@ varbvs <- function (X, Z, y, family = c("gaussian","binomial"), sigma, sa,
   if (verbose) {
     cat("Welcome to           ")
     cat("--       *                              *               \n")
-    cat("VARBVS version 2.3-1 ")
+    cat("VARBVS version 2.3-4 ")
     cat("--       |              |               |               \n")
     cat("large-scale Bayesian ")
     cat("--       ||           | |    |          || |     |   |  \n")
