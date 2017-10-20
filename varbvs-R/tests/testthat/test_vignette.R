9929a9e38e14f77a4b2bd2c5d66b2de5e7bbfdf1
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-10-20 14:00:51 -0500

    Added test_vignette.R.

---
+++ varbvs-R/tests/testthat/test_vignette.R
@@ -0,0 +1,21 @@
+context("varbvs vignettes")
+test_that("leukemia vignette produces correct results",{
+    library(rmarkdown)
+    
+  # Locate the vignette source file, and check that it exists.  
+  rmd.file <- system.file("doc/leukemia.Rmd",package = "varbvs")
+  expect_gt(nchar(rmd.file),0)
+
+  # Evaluate the code in the leukemia vignette.
+  html.file <- tempfile(pattern = "leukemia",fileext = ".html")
+  render(rmd.file,output_file = html.file)
+
+  # Test 1: The number of nonzero coefficients in the fitted glmnet
+  # model should be greater than the number of variables included in
+  # the varbvs model.
+  expect_gt(sum(coef(fit.glmnet,s = lambda.opt) != 0),sum(fit.varbvs$pip)
+
+  # Test 2: The glmnet and varbvs prediction error should be roughly
+  # the same (5% tolerance).
+  expect_equal(mean(y != y.varbvs),mean(y != y.glmnet),tolerance = 0.05)
+})
