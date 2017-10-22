f25cbd11a9dd5ee4186f03d71823b6bac1d73c47
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-10-22 16:02:09 -0500

    Revised singlesnp.R.

--- varbvs-R/inst/code/singlesnp.R
+++ varbvs-R/inst/code/singlesnp.R
@@ -1,9 +1,24 @@
 # My single-SNP calculations.
 load("../datafiles/Muscle_Skeletal.ACTN3.1Mb.RData")
 storage.mode(X) <- "double"
+n <- nrow(X)
+p <- ncol(X)
 
-out <- remove.covariate.effects(X,Z,y)
+out <- varbvs:::remove.covariate.effects(X,Z,y)
 X   <- out$X
 y   <- out$y
 rm(out)
+
+i <- 2654
+j <- 2656
+
+markers <- setdiff(1:p,c(i,j))
+y0 <- y
+y  <- c(y - X[,markers] %*% with(fit,alpha[markers,1] * mu[markers,1]))
+
+sigma <- fit$sigma[1]
+sa    <- fit$sa[1]
+s     <- sa*sigma/(sa*varbvs:::diagsq(X) + 1)
+mu    <- s*c(y %*% X)/sigma
+logBF <- log(s/(sa*sigma)) + mu^2/s/2
 
