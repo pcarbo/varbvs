769a0e610d789b4e35a07ccca7c9ae1bafa49a8d
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-11-08 17:12:07 -0600

    Improved varbvs.noncenter.cc.R.

--- varbvs-R/demo/varbvs.noncenter.cc.R
+++ varbvs-R/demo/varbvs.noncenter.cc.R
@@ -1,4 +1,3 @@
-
 # This script compares the "zero-centered" model, in which the prior
 # on the regression coefficients is centered at zero, against the
 # "non-centered" model, in which the prior mean is fitted to the
@@ -11,9 +10,9 @@ library(varbvs)
 # -----------------
 n  <- 1400  # Number of samples (subjects).
 p  <- 1000  # Number of variables (genetic markers).
-na <- 10    # Number of markers that affect the binary outcome.
-sa <- 0.2   # Variance of log-odds ratios.
-p1 <- 0.1   # Proportion of subjects that will be cases when all the
+na <- 20    # Number of markers that affect the binary outcome.
+sa <- 0.25  # Variance of log-odds ratios.
+p1 <- 0.01  # Proportion of subjects that will be cases when all the
             # genotypes are zero.
 
 # Names of covariates.
@@ -92,36 +91,10 @@ cat("4. SUMMARIZING NON-ZERO-CENTERED MODEL.\n")
 print(summary(fit2))
 cat("\n")
 
-# COMPARE ESTIMATES AGAINST GROUND-TRUTH
-# --------------------------------------
-# Plot the estimated coefficients against the ground-truth coefficients.
-# It is expected that oefficients near zero will be "shrunk" to zero.
-cat("5. PLOTTING COEFFICIENT ESTIMATES.\n")
-trellis.par.set(par.xlab.text = list(cex = 0.75),
-                par.ylab.text = list(cex = 0.75),
-                axis.text = list(cex = 0.75))
-coef.scatterplot <- function (fit, beta.true) {
-  markers  <- labels(fit)
-  beta.est <- coef(fit)
-  beta.est <- beta.est[markers,ncol(beta.est)]
-  return(xyplot(beta.est ~ beta.true,
-                data.frame(beta.true = beta,beta.est = beta.est),
-                pch = 4,col = "black",cex = 0.6,
-                panel = function(x, y, ...) {
-                  panel.xyplot(x,y,...)
-                  panel.abline(a = 0,b = 1,col = "magenta",lty = "dotted")
-                },
-                scales = list(limits = c(-0.1,1.1)),
-                xlab = "ground-truth regression coefficient",
-                ylab = "estimated regression coefficient"))
-}
-print(coef.scatterplot(fit1,coef(fit2)),split = c(1,1,2,1),more = TRUE)
-print(coef.scatterplot(fit2),split = c(2,1,2,1),more = FALSE)
-
 # EVALUATE MODEL PREDICTIONS
 # --------------------------
 # Evaluate accuracy of the two fitted models.
-cat("6. EVALUATING FITTED MODELS.\n")
+cat("5. EVALUATING FITTED MODELS.\n")
 y1 <- predict(fit1,X,Z,type = "class")
 y2 <- predict(fit2,X,Z,type = "class")
 cat("r^2 between predicted Y and observed Y\n")
