932ec3aebf809bdfbb56d54ec3627d568720ab95
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-11-20 13:49:52 -0600

    Working on finemap.gtex.new.R.

--- varbvs-R/inst/code/finemap.gtex.new.R
+++ varbvs-R/inst/code/finemap.gtex.new.R
@@ -3,20 +3,41 @@ source("varbvsfinemap.R")
 
 # SCRIPT PARAMETERS
 # -----------------
-# TO DO.
+sigma   <- 1            # Variance of residual.
+sa      <- 1            # Prior variance of nonzero effects.
+logodds <- log(3/1000)  # Prior inclusion probability.
+
+# These are the additive effects used to simulate the
+# phenotype data.
+beta                 <- rep(0,1000)
+beta[c(201,562,952)] <- c(1,-1,1)
 
 # LOAD GENOTYPE DATA
 # ------------------
 # Retrieve genotype data for 1,000 SNPs near gene FMO2.
 cat("Loading genotype data.\n")
 load("../datafiles/Thyroid.FMO2.1Mb.RData")
-storage.mode(X) <- "double"
+rm(Z,y)
 X <- X[,3501:4500]
 n <- nrow(X)
 p <- ncol(X)
+storage.mode(X) <- "double"
+
+# Center columns of X.
+X <- scale(X,center = TRUE,scale = FALSE)
 
 # SIMULATE PHENOTYPE DATA
+# -----------------------
 cat("Simulating phenotype data.\n")
+y <- c(X %*% beta + sqrt(sigma)*rnorm(n))
+y <- y - mean(y)
 
-varbvs:::varbvsnormupdate(X,sigma,sa, logodds, xy, d, alpha0, mu0, Xr0, i,
-              algorithm.version = c(".Call","Rcpp","R")) {
+# FIT FULLY-FACTORIZED VARIATIONAL APPROXIMATION
+# ----------------------------------------------
+cat("Fitting fully-factorized variational approximation.\n")
+alpha0 <- runif(p)
+alpha0 <- alpha0/sum(alpha0)
+mu0    <- rnorm(p)
+fit1   <- varbvsnorm(X,y,sigma,sa,logodds,alpha0,mu0,
+                     update.sigma = FALSE,update.sa = FALSE)
+
